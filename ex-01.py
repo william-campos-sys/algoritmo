32afd0ac0887c91fc51cb633fae0accfc2720294
william-campos-sys <williamcampos1985@gmail.com>
2025-03-13 21:50:30 -0300

    Create ex-01.py

---
+++ ex-01.py
@@ -0,0 +1,8 @@
+#Problema: Escreva um algoritmo para ler um valor númerico (do teclado) e escrever (na tela) o seu antecessor
+#1- obter o numero
+#2 - descobrir o antecessor
+# 3- informar o numero
+
+print("informe um numero ")
+numero = int(input())
+print(numero-1)
