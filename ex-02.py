87bc56e1248f923e36b4dfca1ef6bc4cd2a9559b
william-campos-sys <williamcampos1985@gmail.com>
2025-03-13 22:00:25 -0300

    Create ex-02.py

---
+++ ex-02.py
@@ -0,0 +1,5 @@
+#Problema: Escreva um algoritmo para ler as dimensões de um retângulo (base e altura), calcular e escrever a área do retângulo
+base = int(input(" qual a base?"))
+altura = int(input(" qual a altura?"))
+resultado =  base * altura
+print ("a área é: " resultado)
